f77a93c813075c4170dd92e2f373872e7cdbe9d3
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2026-04-07 16:42:44 +0300

    create class User

---
+++ users/models.py
@@ -0,0 +1,78 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def normalize_phone(self, phone: str) -> str:
+        """Простейшая нормализация телефона"""
+        return phone.strip().replace(" ", "")
+
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError("Телефон обязателен")
+
+        phone = self.normalize_phone(phone)
+
+        user = self.model(phone=phone, **extra_fields)
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        if not password:
+            raise ValueError("Суперпользователь должен иметь пароль")
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Суперпользователь должен иметь is_superuser=True")
+
+        return self.create_user(phone, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(
+        max_length=20,
+        unique=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name="Имя"
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name="Фамилия"
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.phone or "No phone"
