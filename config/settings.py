821d6182b2c0ecb814cf78f0e626cb00dd1277a4
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2026-04-15 23:38:06 +0300

    add CI with GitHub Actions

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 
 from pathlib import Path
@@ -116,3 +117,12 @@ SIMPLE_JWT = {
 }
 
 SWAGGER_USE_COMPAT_RENDERERS = False
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3",
+        }
+    }
+
